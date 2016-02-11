81b5d5da444b8c36f8cf1c0839f47f7c8c11fc86
Matt Sonnhalter <matthew9559@gmail.com>
2016-02-11 00:46:35 -0600

    Added code from kata

--- SalesmanTravel/SalesmanTravel/Program.cs
+++ SalesmanTravel/SalesmanTravel/Program.cs
@@ -13,3 +13,31 @@ namespace SalesmanTravel
         }
     }
 }
+
+
+
+
+//using System;
+
+//public class SalesmanTravel
+//{
+//    public static string Travel(string r, string zipcode)
+//    {
+//        List<string> zipAndState = new List<string>();
+
+//        string[] addresses = r.Split(',');
+
+//        foreach (var a in addresses)
+//        {
+//            zipAndState.Add(a.Substring(a.Length - 8));
+//        }
+
+//        foreach (var a in addresses)
+//        {
+//            if (zipcode == zipAndState)
+//            {
+
+//            }
+//        }
+//    }
+//}
---
+++ SalesmanTravel/SalesmanTravel/SalesmanTravel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesmanTravel
+{
+    public class SalesmanTravel
+    {
+        public static string Travel(string r, string zipcode)
+        {
+            List<string> zipAndState = new List<string>();
+
+            string[] addresses = r.Split(',');
+
+            foreach (var a in addresses)
+            {
+                zipAndState.Add(a.Substring(a.Length - 8));
+            }
+
+            foreach (var a in addresses)
+            {
+                if (zipcode == zipAndState)
+                {
+
+                }
+            }
+        }
+    }
+}
